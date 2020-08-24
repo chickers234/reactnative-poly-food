249bd3c3102b2aeb7cfef2a643c0e2be14a56add
Tai Ngo <tantai23493@gmail.com>
2020-08-25 03:42:20 +0700

    FCM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import RootStack from './navigation/RootStack';
 import StoreProvider from './utils/store';
+import {Alert} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert(
+        'A new FCM message arrived!',
+        JSON.stringify(remoteMessage.notification.body),
+      );
+      console.log(remoteMessage);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      getFcmToken();
+      console.log('Authorization status:', authStatus);
+    }
+  };
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log(fcmToken);
+      console.log('Your Firebase Token is:', fcmToken);
+    } else {
+      console.log('Failed', 'No token received');
+    }
+  };
+
   return (
     <StoreProvider>
       <RootStack />
